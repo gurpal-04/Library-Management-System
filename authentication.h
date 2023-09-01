a68b3f6c2c8c2054a4c6950055aee9db87da72f0
gurpal-04 <gurpal1705.be21@chitkara.edu.in>
2023-09-01 21:54:00 +0530

    initial commit

---
+++ authentication.h
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+bool emailValidation(const char *str);
+void signIn();
+bool adminLogIn(char username[50], char password[17]);
+bool userLogIn(char username[50], char password[17]);
+void logIn();
---
+++ operations.h
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+void addBook();
+void findBookBy(char findBy[]);
+void findBook();
+void display();
+void userDashboard(char username[]);
+void adminDashboard();
+bool isAvailable(int bookId);
+void issue();
+void returnBook();
+void decrementStock(int bookId);
+void incrementStock(int bookId);
+void myBooks(char username[]);
