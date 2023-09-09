aba9c2a772892f6a28d6a36163eb636de0404032
gurpal-04 <gurpal1705.be21@chitkara.edu.in>
2023-09-09 11:17:25 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 #include "authentication.h"
 
 int main(){
-    printf("/nIf you are new user please sign in.\n");
+    printf("\nIf you are new user please sign in.\n");
     printf("1. Log In\n");
     printf("2. Sign In\n");
 
-    int n;
+    char n;
     printf("Enter your option.\n");
-    scanf("%d",&n);
+    scanf("%s",&n);
 
-    if(n==1){
+    if(n=='1'){
         logIn();
-    }else if (n ==2 ){
+    }else if (n =='2' ){
         signIn();
         logIn();
     }else{
